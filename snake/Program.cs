b29eb53509d8e73fb70d7667ae7ce0a5cd693372
vasilyevmaxv <vasilyevmaxv@gmail.com>
2016-06-16 16:54:30 +0400

    Классы

--- snake/Program.cs
+++ snake/Program.cs
@@ -10,26 +10,20 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            int x1 = 5;
-            int y1 = 10;
-            char sym1 = '*';
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sym = '*';
+            p1.Draw();
 
-            Draw(x1, y1, sym1);
-
-            int x2 = 2;
-            int y2 = 5;
-            char sym2 = '#';
-
-            Draw(x2, y2, sym2);
+            Point p2 = new Point();
+            p2.x = 4;
+            p2.y = 6;
+            p2.sym = '%';
+            p2.Draw();
 
             Console.ReadLine();
 
-        }
-
-        static void Draw(int x,int y,char sym)
-        {
-            Console.SetCursorPosition(x, x);
-            Console.Write(sym);
         }
     }
 }
