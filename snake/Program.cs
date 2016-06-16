38bcd2ff9613d8ab3ea89dc554eb07c9d4080ae7
vasilyevmaxv <vasilyevmaxv@gmail.com>
2016-06-16 16:33:11 +0400

    Функции

---
+++ snake/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x1 = 5;
+            int y1 = 10;
+            char sym1 = '*';
+
+            Draw(x1, y1, sym1);
+
+            int x2 = 2;
+            int y2 = 5;
+            char sym2 = '#';
+
+            Draw(x2, y2, sym2);
+
+            Console.ReadLine();
+
+        }
+
+        static void Draw(int x,int y,char sym)
+        {
+            Console.SetCursorPosition(x, x);
+            Console.Write(sym);
+        }
+    }
+}
